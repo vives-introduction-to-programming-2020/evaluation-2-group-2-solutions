664a245fb52537269edd44d4e43ff378d0342b19
Nico De Witte <nico.dewitte@outlook.com>
2021-01-13 08:31:52 +0100

    Solve StringOfSums

--- StringOfSums/StringOfSums/SumParser.cs
+++ StringOfSums/StringOfSums/SumParser.cs
@@ -17,7 +17,43 @@ namespace StringOfSums
             //
             // "text" contains valid value (not empty, with plus sign and two positive integers)
 
-            return -1;    // TODO - Change this with the real result
+            //////////////////////////////////
+            /// Option 1 - Basic Construct ///
+            //////////////////////////////////
+
+            //// Find the plus sign
+            //int iPlus = 0;
+            //while (text[iPlus] != '+')
+            //{
+            //    iPlus++;
+            //}
+
+            //// Determine left part
+            //string left = "";
+            //for (int i = 0; i < iPlus; i++)
+            //{
+            //    left += text[i];
+            //}
+
+            //// Determine right part
+            //string right = "";
+            //for (int i = iPlus + 1; i < text.Length; i++)
+            //{
+            //    right += text[i];
+            //}
+
+
+            ///////////////////////////////////////
+            /// Option 2 - Using String methods ///
+            ///////////////////////////////////////
+
+            int iPlus = text.IndexOf('+');
+            string left = text.Substring(0, iPlus);
+            string right = text.Substring(iPlus+1, text.Length-(iPlus+1));
+
+
+
+            return Convert.ToInt32(left) + Convert.ToInt32(right);
         }
     }
 }
